befe0cd3bda6e5ed62b06761344cbd4acc4331bc
Guilherme Bornia <guilhermeborniamiranda@gmail.com>
2017-12-05 17:52:27 -0200

    Bloqueando inclusão de usuário já cadastrados

--- Seção 17 - PHP & MySQL, Projeto Twitter Clone/registra_usuario.php
+++ Seção 17 - PHP & MySQL, Projeto Twitter Clone/registra_usuario.php
@@ -8,15 +8,37 @@ $con = (new db())->conecta_mysql();
 // Organiza as informações do novo usuário, criptografando a senha
 $user_infos = array($_POST['usuario'], $_POST['email'], md5($_POST['senha']));
 
-// Prepara a query
-$sql = "INSERT INTO usuarios (usuario, email, senha) VALUES ('$user_infos[0]', '$user_infos[1]', '$user_infos[2]');";
+/* Verifica se o usuário ou o e-mail já existem */
 
-// Executa e verifica se a query foi executada com sucesso
-if(!mysqli_query($con, $sql)) {
-	exit('Erro ao cadastrar novo usuário!');
+// Prepara a query
+$sql = "SELECT usuario, email FROM usuarios WHERE usuario = '$user_infos[0]' OR email = '$user_infos[1]';";
+
+// Executa a query
+$data = mysqli_query($con, $sql);
+
+// Verifica se houve algum retorno, isto é, se já existe o usuário ou o e-mail digitado
+if(mysqli_fetch_array($data, MYSQLI_ASSOC)) {
+	echo 'Usuário e/ou e-mail já cadastrados.';
+
+	// Redireciona para uma página após 2 segundos
+	header('Refresh: 2; url= inscrevase.php');
 } else {
-	echo 'Cadastro realizado com sucesso!';
-	header('Refresh: 2, url= index.php');
+	/*Inclui um novo usuário */
+
+	// Prepara a query
+	$sql = "INSERT INTO usuarios (usuario, email, senha) VALUES ('$user_infos[0]', '$user_infos[1]', '$user_infos[2]');";
+
+	// Executa e verifica se a query foi executada com sucesso
+	if(!mysqli_query($con, $sql)) {
+		exit('Erro ao cadastrar novo usuário!');
+	} else {
+		echo 'Cadastro realizado com sucesso!';
+
+		//// Redireciona para uma página após 2 segundos
+		header('Refresh: 2, url= index.php');
+	}
 }
+
+
 
 ?>
