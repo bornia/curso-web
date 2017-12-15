955dbbf6fc1cec9adfb326d3e503977cc282bba5
Guilherme Bornia <guilhermeborniamiranda@gmail.com>
2017-12-15 19:13:19 -0200

    Controlando a exibição dos botões de seguir e deixar de seguir

--- Seção 17 - PHP & MySQL, Projeto Twitter Clone/get_pessoas.php
+++ Seção 17 - PHP & MySQL, Projeto Twitter Clone/get_pessoas.php
@@ -13,21 +13,34 @@ $con = (new db())->conecta_mysql();
 $user_infos = array($_POST['nome_pessoa'], $_SESSION['id_usuario']);
 
 // Prepara a query
-$sql = "SELECT * FROM usuarios WHERE usuario LIKE '%$user_infos[0]%' AND id <> $user_infos[1];";
+$sql = "SELECT u.*, us.* FROM usuarios AS u LEFT JOIN usuarios_seguidores AS us ON (us.id_usuario = $user_infos[1] AND u.id = us.seguindo_id_usuario) WHERE u.usuario LIKE '%$user_infos[0]%' AND u.id <> $user_infos[1];";
 
 // Verifica se a query foi executada com sucesso
 if ($res = mysqli_query($con, $sql)) {
 	// Varre a tabela usuarios
 	while ($data = mysqli_fetch_array($res, MYSQLI_ASSOC)) {
+		// Define se o usuário está sendo seguido ou não
+		$esta_seguindo_usuario_sn = isset($data['id_usuario_seguidor']) && !empty($data['id_usuario_seguidor']) ? TRUE : FALSE;
+
+		// Verifica se o usuário está sendo seguido
+		if ($esta_seguindo_usuario_sn) {
+			$btn_deixar_seguir_display = 'block';
+			$btn_seguir_display = 'none';
+		}
+		else {
+			$btn_deixar_seguir_display = 'none';
+			$btn_seguir_display = 'block';
+		}
+
 		echo
 			"<a href='#' class='list-group-item'> " .
 				"<strong> " . $data['usuario'] . " </strong> " . 
 				"<small> " . $data['email'] . " </small> " .
 				"<p class='list-group-item-text pull-right'> " .
-					"<button type='button' id='btn_seguir_".$data['id']."' class='btn btn-default btn_seguir' data-id_usuario='".$data['id']."'> " .
+					"<button type='button' id='btn_seguir_".$data['id']."' class='btn btn-default btn_seguir' data-id_usuario='".$data['id']."' style='display: ".$btn_seguir_display.";'> " .
 						"Seguir " .
 					"</button> " .
-					"<button type='button' id='btn_deixar_seguir_".$data['id']."' class='btn btn-primary deixar_btn_seguir' data-id_usuario='".$data['id']."' style='display: none;'> " .
+					"<button type='button' id='btn_deixar_seguir_".$data['id']."' class='btn btn-primary deixar_btn_seguir' data-id_usuario='".$data['id']."' style='display: ".$btn_deixar_seguir_display.";'> " .
 						"Deixar de Seguir " .
 					"</button> " .
 				"</p> " .
